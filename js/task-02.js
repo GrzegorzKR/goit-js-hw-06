d1f9c9f4b41f11d8d420ce227c3b7dd5b4e3f7cf
GrzegorzKR <91796260+GrzegorzKR@users.noreply.github.com>
2022-10-27 21:49:07 +0200

    start Task2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,9 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const $ul = document.querySelector("ul");
+console.log($ul);
+
+const newItem = document.createElement("li");
+$ul.append(newItem, newItem, newItem);
