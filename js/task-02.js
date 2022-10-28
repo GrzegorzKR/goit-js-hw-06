7077a8de42d2fe90b813df83918bd7b15bcbd126
GrzegorzKR <91796260+GrzegorzKR@users.noreply.github.com>
2022-10-28 20:04:57 +0200

    Task 2 complete

--- js/task-02.js
+++ js/task-02.js
@@ -1,14 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
 
-const $ul = document.querySelector("ul");
-console.log($ul);
+const $ul = document.querySelector(`#ingredients`);
 
-const newItem = document.createElement("li");
-$ul.append(newItem, newItem, newItem);
+for (let i = 0; i < ingredients.length; i += 1) {
+  const newItem = document.createElement("li");
+  newItem.classList.add("item");
+  newItem.textContent = ingredients[i];
+  $ul.append(newItem);
+}
