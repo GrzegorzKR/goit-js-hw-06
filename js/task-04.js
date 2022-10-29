96f129a73e2430480c17e189adfc067097edd404
GrzegorzKR <91796260+GrzegorzKR@users.noreply.github.com>
2022-10-29 17:55:24 +0200

    Task 4 complete

--- js/task-04.js
+++ js/task-04.js
@@ -1,6 +1,19 @@
-let counterValue = 0;
+const subtractBtn = document.querySelector(`[data-action="decrement"]`);
+const addBtn = document.querySelector(`[data-action="increment"]`);
 
-const addBtn = document.querySelector(`[data-action="decrement"]`);
-const subtractBtn = document.querySelector(`[data-action="increment"]`);
+let defaultValue = document.querySelector(`#value`);
+let counterValue = Number(defaultValue.innerHTML);
+console.log(counterValue);
 
+const add = () => {
+  counterValue = counterValue += 1;
+  defaultValue.textContent = counterValue;
+};
 
+const substract = () => {
+  counterValue = counterValue -= 1;
+  defaultValue.textContent = counterValue;
+};
+
+addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", substract);
