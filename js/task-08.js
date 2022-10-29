09be364a2b38b14afc2f14b17f1971abbf993cc7
GrzegorzKR <91796260+GrzegorzKR@users.noreply.github.com>
2022-10-29 22:01:43 +0200

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const $form = document.querySelector(".login-form");
 
+const submitForm = (event) => {
+  const $form = event.currentTarget;
+  event.preventDefault();
+};
+
+$form.addEventListener("submit", submitForm);
+
+console.log($form.elements);
+
+const {
+  elements: { email, password },
+} = $form;
+
+console.log(email);
+console.log(password);
