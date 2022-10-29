bc923a6c9b93b48679f08b6bc357d3d788318020
GrzegorzKR <91796260+GrzegorzKR@users.noreply.github.com>
2022-10-29 19:51:20 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,6 +1,4 @@
 const $textInput = document.querySelector(`#name-input`);
-console.log($textInput);
-
 const $textOutput = document.querySelector(`#name-output`);
 
 $textInput.addEventListener("input", (event) => {
