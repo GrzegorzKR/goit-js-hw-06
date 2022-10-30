fa4e797c39b29ce7a79dfec366641169fb00a9d2
GrzegorzKR <91796260+GrzegorzKR@users.noreply.github.com>
2022-10-30 19:39:33 +0100

    Task 9 complete

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const $body = document.querySelector("body");
+const $btnColor = document.querySelector(".change-color");
+const $spanColor = document.querySelector(".color");
+
+const colorChange = () => {
+  $body.style.backgroundColor = getRandomHexColor();
+  $spanColor.textContent = getRandomHexColor();
+};
+
+$btnColor.addEventListener("click", colorChange);
