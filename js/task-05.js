3557dc10727520eb4d892c2c9de7299ff16be959
GrzegorzKR <91796260+GrzegorzKR@users.noreply.github.com>
2022-10-31 21:03:13 +0100

    update task 5

--- js/task-05.js
+++ js/task-05.js
@@ -2,7 +2,7 @@ const $textInput = document.querySelector(`#name-input`);
 const $textOutput = document.querySelector(`#name-output`);
 
 $textInput.addEventListener("input", (event) => {
-  if (event.currentTarget.value === "") {
-    $textOutput.textContent = "Anonymous";
-  } else $textOutput.textContent = event.currentTarget.value;
+  event.currentTarget.value === ""
+    ? ($textOutput.textContent = "Anonymous")
+    : ($textOutput.textContent = event.currentTarget.value);
 });
