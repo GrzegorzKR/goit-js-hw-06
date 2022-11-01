ec29841166fef0d49acb1d3d9b0c86a888cfe78d
GrzegorzKR <91796260+GrzegorzKR@users.noreply.github.com>
2022-11-01 19:42:14 +0100

    Task6 corrected

--- js/task-06.js
+++ js/task-06.js
@@ -2,11 +2,11 @@ const $input = document.querySelector("input");
 const $dataLength = $input.dataset.length;
 
 const formValidation = (event) => {
-  if (event.currentTarget.value.length < $dataLength) {
+  if (event.currentTarget.value.length != $dataLength) {
     $input.classList.add("invalid");
   } else {
-    $input.classList.add("valid");
     $input.classList.remove("invalid");
+    $input.classList.add("valid");
   }
 };
 
